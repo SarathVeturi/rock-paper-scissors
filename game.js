44bdda479927eb6afc1a5c1c6c6a2bceb4d2350b
SarathVeturi <sarathveturi99@gmail.com>
2020-08-17 00:15:24 +0530

    added style.css and modified game.js, index.html

---
+++ game.js
@@ -0,0 +1,83 @@
+let choices=["rock", "paper", "scissors"];
+let para=document.querySelector("p");
+let myButton=document.querySelector("button");
+let computer_score=0, player_score=0;
+let comp=document.getElementById("comp");
+let play=document.getElementById("play");
+let playerSelection1;
+function computerPlay()
+{
+    return Math.floor(Math.random()*3);
+}
+
+function playRound()
+{
+    playerSelection=prompt("Enter your choice").toLowerCase();
+    if(playerSelection!=="rock" && playerSelection!=="paper" && playerSelection!=="scissors")
+    {
+        alert("enter any one of rock, paper, scissors(CASE-INSENSITIVE)");
+        playerSelection1=prompt("Enter your choice").toLowerCase();
+    }
+    let computerSelection=choices[computerPlay()].toLowerCase();
+    while(computerSelection===playerSelection)
+        {
+            computerSelection=choices[computerPlay()].toLowerCase();
+        }
+    console.log(`Computer chose ${computerSelection}`);
+    if(playerSelection==="rock" && computerSelection==="paper")
+        //console.log("You lose...You are wrapped by the paper!");
+        {
+            para.textContent="You lose...You are wrapped by the paper!";
+            computer_score++;
+            comp.innerHTML=computer_score;
+            play.innerHTML=player_score;
+        }
+    else if(playerSelection==="rock" && computerSelection==="scissors")
+        //console.log("You win...Rock beats the scissors!");
+        {
+            para.textContent="You win...Rock beats the scissors!";
+            player_score++;
+            comp.innerHTML=computer_score;
+            play.innerHTML=player_score;
+        }
+    else if(playerSelection==="paper" && computerSelection==="rock")
+        //console.log("You win...Paper wraps the rock");
+        {
+            para.textContent="You win...Paper wraps the rock";
+            player_score++;
+            comp.innerHTML=computer_score;
+            play.innerHTML=player_score;
+        }
+    else if(playerSelection==="paper" && computerSelection==="scissors")
+        //console.log("You lose...You are cut by the scissors");
+        {
+            para.textContent="You lose...You are cut by the scissors";
+            computer_score++;
+            comp.innerHTML=computer_score;
+            play.innerHTML=player_score;
+        }
+    else if(playerSelection==="scissors" && computerSelection==="rock")
+        //console.log("You lose...You are beaten by the rock!");
+        {
+            para.textContent="You lose...You are beaten by the rock!";
+            computer_score++;
+            comp.innerHTML=computer_score;
+            play.innerHTML=player_score;
+        }
+    else if(playerSelection==="scissors" && computerSelection==="paper")
+        //console.log("You win...You have cut the paper!");
+        {
+            para.textContent="You win...You have cut the paper!";
+            player_score++;
+            comp.innerHTML=computer_score;
+            play.innerHTML=player_score;
+        }
+    game();
+}
+
+myButton.onclick=function game()
+{
+playRound();
+}
+
+game();
